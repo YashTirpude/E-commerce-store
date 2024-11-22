691e8b3849d250afc174dacce7eb459043b6f043
Yash <137068938+YashTirpude@users.noreply.github.com>
2024-11-22 14:38:19 +0530

    handle responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 import daisyui from "daisyui";
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "375px",
+      // => @media (min-width: 640px) { ... }
+      sm: "640px",
+      // => @media (min-width: 768px) { ... }
+      md: "768px",
+      // => @media (min-width: 1024px) { ... }
+      lg: "1024px",
 
+      laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
   daisyui: {
     themes: [
       "light",
